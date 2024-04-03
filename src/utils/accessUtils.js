5f5e5e00574a5887043b0f1e446bbf6b9ad2a2b2
Esteban Santini <esantinie@gmail.com>
2024-04-03 02:42:27 -0700

    notify when unauthorized access

--- src/utils/accessUtils.js
+++ src/utils/accessUtils.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const { sendEmail } = require('./utils');
 
 exports.getIsWhitelisted = (req) => {
   const { token } = req.cookies;
@@ -17,5 +18,26 @@ exports.getIsAdmin = (req) => {
   const { admin } = config.oauth;
   const { email } = jwt.verify(token, config.tokenSecret);
 
-  return admin.indexOf(email) !== -1;
+  const isAdmin = admin.indexOf(email) !== -1;
+
+  if (!isAdmin) {
+    console.error(`Unauthorized Access by ${req.ip}`);
+    const geo = geoip.lookup(req.ip);
+    sendEmail({
+      subject: 'Unauthorized Access Attempt',
+      text: `
+        Unauthorized Access from <b>${req.ip}</b> <br /><br />
+        IP Address: <b>${req.ip}</b> <br /><br />
+        Timestamp: <b>${new Date().toISOString()}</b> <br /><br />
+        Requested URL: <b>${req.originalUrl}</b> <br /><br />
+        HTTP Method: <b>${req.method}</b> <br /><br />
+        User Agent: <b>${req.headers['user-agent']}</b> <br /><br />
+        Request Body: <b>${JSON.stringify(req.body)}</b> <br /><br />
+        Referrer: <b>${req.headers['referrer']}</b> <br /><br />
+        Request Headers: <b>${JSON.stringify(req.headers)}</b> <br /><br />
+        Geo: <b>${JSON.stringify(geo)}</b>
+    ` });
+  }
+
+  return isAdmin;
 }
